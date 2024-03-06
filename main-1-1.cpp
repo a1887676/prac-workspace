d3a7dd6739b78a7b7a8165a2c1ba31c87b1f4c5f
a1887676 <a1887676@adelaide.edu.au>
2024-03-06 17:29:58 +1030

    completed 1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
-int array_sum(int array[], int n);
-
 int main() {
 
-    int mainArray[] = {1, 2, 3, 4, 5};
-    int sizeArray = sizeof(mainArray)/sizeof(mainArray[0]);
-    
+extern int sum_diagonal(int array[4][4]);
 
-    int mainSum = array_sum(mainArray, sizeArray);
+int mainArray[4][4] = {
+{1,2,3,4},
+{5,6,7,8},
+{9,10,11,12},
+{13,14,15,16} };
 
-    std::cout << "Sum of array: " << mainSum << std::endl;
+int result = sum_diagonal(mainArray);
 
-    return 0;
+std::cout << "diagonal sum is: " << result << std::endl;
+return 0;
 }
--- main-3-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-
-int main() {
-
-extern void print_pass_fail(char grade);
-print_pass_fail('A');
-
-return 0;
-}
