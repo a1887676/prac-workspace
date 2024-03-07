ab23f7ccc101e19e7038f31f059a3e45005a392b
a1887676 <a1887676@adelaide.edu.au>
2024-03-07 16:48:46 +1030

    completed 2-1

---
+++ function-2-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void print_binary_str(std::string decimal_number) {
+
+int base_tenNum = std::stoi(decimal_number);
+int base = 2;
+int base_twoArray[32];
+int counter = 0;
+
+while(base_tenNum != 1/2 || base_tenNum !=0 ) {
+
+if(base_tenNum % base == 0) {   
+base_twoArray[counter] = 0;
+}
+
+if(base_tenNum % base == 1) {   
+base_twoArray[counter] = 1;  
+}
+
+
+base_tenNum = base_tenNum / 2;
+counter++;
+ }
+
+for(int i=0;i<counter;i++) {
+
+std::cout << base_twoArray[i];
+
+}
+std::cout << std::endl; 
+return 0;
+}
---
+++ main-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+extern void print_binary_str(std::string decimal_number);
+
+int string = '37';
+
+print_binary_str(string);
+return 0;    
+}
