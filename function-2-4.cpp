4d2538c981a7caf74787982a5db1cedd092b5340
a1887676 <a1887676@adelaide.edu.au>
2024-03-08 12:37:20 +1030

    completed 2-4

---
+++ function-2-4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int sum_min_max(int integers[], int length) {
+
+extern int array_min(int integers[], int length);
+extern int array_max(int integers[], int length);
+
+if(length<1) {
+    return -1;
+}
+
+int min = array_min(integers,length);
+int max = array_max(integers,length);
+
+int sum = min+max;
+
+return sum;
+}
+
+
+
+int array_min(int integers[], int length) {
+
+int min = integers[0];
+for(int i=1;i<length;i++) {
+    if(integers[i]<min) {
+        min = integers[i];
+    }
+}
+
+return min;
+}
+
+
+
+int array_max(int integers[], int length) {
+
+int max = integers[0];
+for(int i=1;i<length;i++) {
+    if(integers[i]>max) {
+        max = integers[i];
+    }
+}
+
+return max;
+}
