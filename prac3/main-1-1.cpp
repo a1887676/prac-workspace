c0f091524e35f00b02782320358bb604072817a2
a1887676 <a1887676@adelaide.edu.au>
2024-03-20 17:21:06 +1030

    2change 1-1

--- prac3/main-1-1.cpp
+++ prac3/main-1-1.cpp
@@ -12,7 +12,11 @@ int size;
 std::cin >> size;
 std::cout << std::endl;
 
-createPersonArray(size);
+Person* people = createPersonArray(size);
+
+for (int i=0; i<size; i++) {
+        std::cout << "Name - " << people[i].name << ", Age - " << people[i].age << std::endl;
+    }
 
 return 0;
 }
