393a1c740569bc4c769760023254cb0a0efaba24
a1887676 <a1887676@adelaide.edu.au>
2024-03-22 11:32:30 +1030

    1change 1-3

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -14,10 +14,10 @@ std::cin >> size;
 std::cout << std::endl;
 
 PersonList list1 = createPersonList(size);
-Person listDeepcopy = deepCopyPersonList(list1);
+PersonList deepCopy = deepCopyPersonList(list1);
 
 for (int i=0; i<size; i++) {
-        std::cout << "Name - " << list1.people[i].name << ", Age - " << list1.people[i].age << std::endl;
+        std::cout << "Name - " << deepCopy.people[i].name << ", Age - " << deepCopy.people[i].age << std::endl;
     }
 
 return 0;
