16b78ca6fb8f85985244e9eb4a21bf5c68628ae8
a1887676 <a1887676@adelaide.edu.au>
2024-03-22 11:48:27 +1030

    completed 1-4

---
+++ function-1-4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+#include "Person.h"
+
+PersonList createPersonList(int n) {
+
+PersonList list1;
+
+list1.numPeople = n;
+list1.people = new Person[n];
+
+for(int i=0;i<n;i++) {
+    list1.people[i].name = "Jane Doe";
+    list1.people[i].age = 1;
+}
+
+return list1;    
+}
+
+PersonList shallowCopyCopyPersonList(PersonList pl) {
+    PersonList shallowCopy;
+    PersonList* pl;
+
+shallowCopy.numPeople = pl.numPeople;
+shallowCopy.people = pl;
+
+//for(int i=0;i<deepCopy.numPeople;i++) {
+  //  deepCopy.people[i] = pl.people[i];
+//}
+
+return deepCopy;
+}
