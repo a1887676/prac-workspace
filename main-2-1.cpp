a3a1396d0dcefc3269bf1ebbd8f3aa00c41e49df
a1887676 <a1887676@adelaide.edu.au>
2024-05-02 17:50:07 +0930

    changes 4

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -11,7 +11,7 @@ Tesla tesla1(x,50);
 tesla1.chargeBattery(2);
 tesla1.drive(5);
 
-std::cout << "The Tesla model: " << tesla1.get_model() << " Battery: " << tesla1.get_batteryPercentage() << " emissions: " << tesla1.get_emissions << std::endl; 
+std::cout << "The Tesla model: " << tesla1.get_model() << " Battery: " << tesla1.get_batteryPercentage() << std::endl; 
 
 return 0;
 }
