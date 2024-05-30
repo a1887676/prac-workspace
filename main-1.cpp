7dfe40713d11bcfc1ea2d49b431774a191a83588
a1887676 <a1887676@adelaide.edu.au>
2024-05-30 16:31:12 +0930

    2nd commit

--- main-1.cpp
+++ main-1.cpp
@@ -5,8 +5,8 @@
 
 int main(){
 
-    std::tuple<int, int> coords1 = Helper::generateRandomPos(10, 10);
-    std::tuple<int, int> coords2 = Helper::generateRandomPos(10, 10);
+    std::tuple<int, int> coords1 = Helper::generateRandomCoordinates(10, 10);
+    std::tuple<int, int> coords2 = Helper::generateRandomCoordinates(10, 10);
 
     double distance = Helper::calculateDistance(coords1, coords2);
     std::cout << "Distance between 1 and 2: " << distance << std::endl;
